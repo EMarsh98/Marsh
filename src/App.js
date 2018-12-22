2ade433f161715f28b2f00bc2517b61dd142039d
EMarsh98 <44880914+EMarsh98@users.noreply.github.com>
2018-12-22 19:06:46 -0400

    More

--- src/App.js
+++ src/App.js
@@ -21,17 +21,35 @@ function About(){
 	);
 }
 
+function FoodBank(){
+	return(
+		<div className="container">
+		<div className="row">
+			<div className="col">
+				<h1>The Glace Bay Food Bank</h1>
+				<h4>This was a project assigned to myself and a team of other students at NSCC. We had to develop a website for a local foodbank. While I helped create the design of the site, my main responsibility was to make the site mobile friendly</h4>
+				<a href="http://glacebayfoodbank.seaside.ns.ca" className="btn btn-primary">Check it out</a>
+			</div>
+		</div>
+		</div>
+	);
+}
+
 class App extends Component {
 	constructor(props){
 		super(props);
-		this.state = {value : 0};
+		this.state = {tab : 0};
 		
 		this.changeTab = this.changeTab.bind(this);
 	}
 	
+	changeTab(num){
+		this.setState({tab : num});
+	}
+	
 	render() {
 		return (
-			<div className="container-fluid">
+			<div id="main" className="container-fluid">
 			<div className="row">
 				<div className="col">
 					<h1>Marsh Media</h1>
@@ -40,12 +58,13 @@ class App extends Component {
 			<div className="row">
 				<div className="col-lg-2 bg-primary text-white">
 					<ul className="nav flex-column">
-						<li className="nav-item" >About Myself</li>
-						<li className="nav-item" >Glace Bay Food Bank</li>
+						<li className={this.state.tab === 0 ? "nav-item select" : "nav-item"} onClick={() => this.changeTab(0)} >About Myself</li>
+						<li className={this.state.tab === 1 ? "nav-item select" : "nav-item"} onClick={() => this.changeTab(1)}>Glace Bay Food Bank</li>
 					</ul>
 				</div>
-				<div className="col-lg-10 bg-secondary text-white">
-					<About />
+				<div id="main" className="col-lg-10 bg-secondary text-white">
+					{this.state.tab === 0 ? <About /> : null}
+					{this.state.tab === 1 ? <FoodBank /> : null}
 				</div>
 			</div>
 			</div>
